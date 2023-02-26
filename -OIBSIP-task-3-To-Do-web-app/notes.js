bc6ce94f3532be0443a819fd73085c3fb2b31c70
Anuja Khanaj <102312116+Anuja-Khanaj@users.noreply.github.com>
2023-02-26 16:45:30 +0530

    Add files via upload

---
+++ -OIBSIP-task-3-To-Do-web-app/notes.js
@@ -0,0 +1,55 @@
+console.log("Welcome to notes");
+showNotes();
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    showNotes();
+ 
+});
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `
+      <tr><p>${index+1}) ${element}
+    </p>
+    <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button></tr>`;
+  });
+
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+  }
+}
+
+function deleteNote(index) {
+  console.log("deleting the note.", index);
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
+
